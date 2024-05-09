a28375adf2911e844e59296c87bc3ec87fcb3f17
hyeongseopE <gudtjq363636@naver.com>
2024-05-09 18:45:38 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner ip = new Scanner(System.in);
+        int a,b;
+        a = ip.nextInt();
+        b = ip.nextInt();
+        System.out.println(a+b);
+    }
+}
