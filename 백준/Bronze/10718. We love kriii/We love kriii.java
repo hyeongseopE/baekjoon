7f64272c67e0faa97c6849bd8df88da9a8e7dbd3
hyeongseopE <gudtjq363636@naver.com>
2024-05-24 23:17:29 +0900

    [Bronze V] Title: We love kriii, Time: 108 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+
+    }
+}
