1f07da27a51e7a0abc876011093cba70eceb427a
hyeongseopE <gudtjq363636@naver.com>
2024-06-15 02:30:14 +0900

    [Bronze V] Title: 아!, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        if(s1.length() >= s2.length()){
+            System.out.println("go");
+        }else{
+            System.out.println("no");
+        }
+
+
+        
+    }
+}
