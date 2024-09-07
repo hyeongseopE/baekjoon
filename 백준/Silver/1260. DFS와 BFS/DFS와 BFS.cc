9c658f8ff0fdcf3fc9e6a5d6b24858deb8389bbc
hyeongseopE <gudtjq363636@naver.com>
2024-09-07 10:41:18 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 1404 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <vector>
 #include <algorithm>
 #include <cstring>
 using namespace std;
 int m,n,v,ix,iy,dcnt;
-int map[1001][1001];
-int chk[1001];
-int ans[1001],ans2[1001];
+vector<int> vec[1010];
+int chk[1010];
+int ans[1010],ans2[1010];
 
-int vitsit[1001];
-int q[1001];
+int vitsit[1010];
+int q[1010];
 int top,poll,c;
 void bfs(int x){
     top=poll=-1;
@@ -18,11 +19,12 @@ void bfs(int x){
     while(top != poll){
         int tx = q[++poll];
 
-        for (int i = 1; i <= m; ++i) {
-            if(map[tx][i] == 1 && !vitsit[i]){
-                q[++top] = i;
-                vitsit[i] = 1;
-                ans2[++c] = i;
+        for (int i = 0; i < vec[tx].size(); ++i) {
+            int b = vec[tx].at(i);
+            if(!vitsit[b]){
+                q[++top] = b;
+                vitsit[b] = 1;
+                ans2[++c] = b;
 
             }
         }
@@ -36,10 +38,10 @@ void bfs(int x){
 void dfs(int d){
     chk[d] = 1;
     ans[dcnt++] = d;
-    if(dcnt > n){return;}
-    for (int i = 1; i <= m; ++i) {
-        if(map[d][i]==1&&!chk[i]){
-            dfs(i);
+    for (int i = 0; i < vec[d].size(); ++i) {
+        int x = vec[d].at(i);
+        if(!chk[x]) {
+            dfs(x);
         }
     }
 
@@ -48,11 +50,17 @@ void dfs(int d){
 int main() {
     scanf("%d%d%d",&m,&n,&v);
 
+
     for (int i = 0; i < n; ++i) {
         scanf("%d%d",&ix,&iy);
-        map[ix][iy] = 1;
-        map[iy][ix] = 1;
+        vec[ix].push_back(iy);
+        vec[iy].push_back(ix);
     }
+    for(int i=1;i<=m;i++)
+    {
+        sort(vec[i].begin(),vec[i].end());
+    }
+
 
 
     dfs(v);
