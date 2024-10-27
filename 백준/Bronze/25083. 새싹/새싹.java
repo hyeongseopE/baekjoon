8c0484f7f92b48a5a03a1ee3bb916e97f8362725
hyeongseopE <gudtjq363636@naver.com>
2024-10-27 17:39:24 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+
+    }
+}
+
+
