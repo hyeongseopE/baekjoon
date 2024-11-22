b727e9f625aea469a9de8b8385a5f86b54ef2887
hyeongseopE <gudtjq363636@naver.com>
2024-11-22 18:46:37 +0900

    [Silver IV] Title: 상근이의 여행, Time: 216 ms, Memory: 30896 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                st = new StringTokenizer(br.readLine());
+            }
+
+            System.out.println(n-1);
+        }
+
+
+
+
+
+    }
+}
