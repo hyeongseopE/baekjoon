8a07324cac810dc604bd56c887a83111ff505048
hyeongseopE <gudtjq363636@naver.com>
2025-01-13 23:54:30 +0900

    [Bronze V] Title: AI 선도대학, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a-2024);
+
+
+    }
+}
