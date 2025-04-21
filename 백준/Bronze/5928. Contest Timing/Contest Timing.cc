374e4bb78807e6aa8f66d14f3050e535531d8a74
hyeongseopE <gudtjq363636@naver.com>
2025-04-22 02:42:44 +0900

    [Bronze IV] Title: Contest Timing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5928. Contest Timing/Contest Timing.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int d, h, m;
+
+    cin >> d >> h >> m;
+
+    int minute = 11 + 11 * 60 + 11 * 1440;
+    int answer = m + h * 60 + d * 1440 - minute;
+
+    cout << (answer < 0 ? -1 : answer);
+
+    return 0;
+}
