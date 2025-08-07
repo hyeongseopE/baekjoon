d8cfe6548c6b662978d29c605acd7a05562f5bb0
형섭 <gudtjq363636@naver.com>
2025-08-07 21:25:48 +0900

    [Bronze IV] Title: 11 月 (November), Time: 176 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/27332. 11 月 （November）/11 月 （November）.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		a = a + b * 7;
+		
+		if(a > 30) {
+			System.out.println("0");
+		}else {
+			System.out.println("1");
+		}
+		sc.close();
+	}
+}
